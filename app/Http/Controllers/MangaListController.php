f54aa690b724b938ec51c72f2fda9b9c9212ea2f
Ridwan <ridwanpr@outlook.com>
2025-02-14 22:04:30 +0700

    cache text list

--- app/Http/Controllers/MangaListController.php
+++ app/Http/Controllers/MangaListController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Genre;
 use App\Models\Manga;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class MangaListController extends Controller
 {
@@ -26,19 +27,21 @@ class MangaListController extends Controller
 
     public function textList()
     {
-        $mangas = Manga::with('detail')
-            ->whereHas('detail')
-            ->select('title', 'slug')
-            ->orderBy('title')
-            ->get()
-            ->groupBy(function ($manga) {
-                $cleanedTitle = preg_replace('/^[^a-zA-Z0-9]+/', '', $manga->title);
-                $firstChar = strtoupper(substr($cleanedTitle, 0, 1));
+        $mangas = Cache::remember('manga.list', now()->addHours(3), function () {
+            return Manga::with('detail')
+                ->whereHas('detail')
+                ->select('title', 'slug')
+                ->orderBy('title')
+                ->get()
+                ->groupBy(function ($manga) {
+                    $cleanedTitle = preg_replace('/^[^a-zA-Z0-9]+/', '', $manga->title);
+                    $firstChar = strtoupper(substr($cleanedTitle, 0, 1));
 
-                return ctype_alpha($firstChar) ? $firstChar : '#';
-            })
-            ->map(fn($titles) => $titles->sortBy('title'))
-            ->sortKeys();
+                    return ctype_alpha($firstChar) ? $firstChar : '#';
+                })
+                ->map(fn($titles) => $titles->sortBy('title'))
+                ->sortKeys();
+        });
 
         return view('manga.list', compact('mangas'));
     }
