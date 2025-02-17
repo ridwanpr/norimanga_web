fa06c59feb9267ecc4327f977231ad9ff29c6288
Ridwan <ridwanpr@outlook.com>
2025-02-17 13:51:24 +0700

    fix migration

--- database/migrations/2025_02_15_031914_create_user_roles_table.php
+++ database/migrations/2025_02_15_031914_create_user_roles_table.php
@@ -23,9 +23,17 @@ return new class extends Migration
             ['name' => 'user'],
         ]);
 
-        Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')->nullable()->default(2);
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
 
         User::create([
