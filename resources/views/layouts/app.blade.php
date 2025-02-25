11ca9244d567f45cfea515efdcdf4a6f7c0d6984
Ridwan <ridwanpr@outlook.com>
2025-02-25 22:12:57 +0700

    add cf turnstile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="en" data-bs-theme="dark">
 
 <head>
-    @turnstileScripts()
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -10,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/css/nori.css', 'resources/js/app.js'])
     @stack('css')
+    @turnstileScripts()
 </head>
 
 <body>
