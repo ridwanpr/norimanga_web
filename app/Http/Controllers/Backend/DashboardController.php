e3fd84b7e81a61c2b0a6dc7c60c80a619c11d1e1
Ridwan <ridwanpr@outlook.com>
2025-02-25 19:38:53 +0700

    fix dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -13,12 +13,12 @@ class DashboardController extends Controller
     {
         $totalUser = User::where('role_id', 2)->count();
         $totalComic = Manga::whereHas('detail')->count();
-        $totalChapter = MangaChapter::where('image', '!=', '[]')->count();
+        $totalChapter = MangaChapter::whereJsonLength('image', '>', 0)->count();
         $totalProject = Manga::where('is_project', true)->count();
 
         $latestUsers = User::where('role_id', 2)->latest()->take(10)->get();
         $latestManga = Manga::whereHas('detail')->latest()->take(10)->get();
-        $latestChapters = MangaChapter::where('image', '!=', '[]')->latest('updated_at')->take(10)->get();
+        $latestChapters = MangaChapter::whereJsonLength('image', '>', 0)->latest('updated_at')->take(10)->get();
 
         return view('backend.dashboard', compact('totalUser', 'totalComic', 'totalChapter', 'latestUsers', 'latestManga', 'latestChapters', 'totalProject'));
     }
