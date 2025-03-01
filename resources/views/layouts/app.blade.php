5553133d860fb3a15f23fc7f928cba92de7dc598
Ridwan <ridwanpr@outlook.com>
2025-03-01 23:54:54 +0700

    Added Google site verification meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="3YJEL3qM5RMzMrxfjcKLNsyOPdDDLwuJkpc1V2RKOuE" />
     <link rel="canonical" href="{{ url()->current() }}">
     @yield('meta')
     <title>@yield('title', 'Nori - Baca Manga, Manhwa, Manhua Bahasa Indonesia')</title>
