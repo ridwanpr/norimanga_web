70cbda46dd4287df505ba063761e408540b4f771
Ridwan <ridwanpr@outlook.com>
2025-03-04 03:48:03 +0700

    Added existence check before creating UserActivity

--- app/Services/UserActivityService.php
+++ app/Services/UserActivityService.php
@@ -9,12 +9,20 @@ class UserActivityService
   public function storeUserActivity(
     int $userId,
     int $mangaId,
-    int $chapterId,
-  ): UserActivity {
-    return UserActivity::create([
+    int $chapterId
+  ): ?UserActivity {
+    if (!UserActivity::where([
       'user_id' => $userId,
       'manga_id' => $mangaId,
       'chapter_id' => $chapterId,
-    ]);
+    ])->exists()) {
+      return UserActivity::create([
+        'user_id' => $userId,
+        'manga_id' => $mangaId,
+        'chapter_id' => $chapterId,
+      ]);
+    }
+
+    return null;
   }
 }
