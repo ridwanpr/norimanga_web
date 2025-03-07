ad7046b602d11c6af29340aef4b5ee0dcb33a9d8
Ridwan <ridwanpr@outlook.com>
2025-03-07 19:56:02 +0700

    add s6 bucket

--- config/filesystems.php
+++ config/filesystems.php
@@ -125,6 +125,18 @@ return [
             'report' => false,
         ],
 
+        's6' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID_6'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY_6'),
+            'region' => env('AWS_DEFAULT_REGION_6'),
+            'bucket' => env('AWS_BUCKET_6'),
+            'url' => env('AWS_URL_6'),
+            'endpoint' => env('AWS_ENDPOINT_6'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+            'report' => false,
+        ],
     ],
 
     /*
